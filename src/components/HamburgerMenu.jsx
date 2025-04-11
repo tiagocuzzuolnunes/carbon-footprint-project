41a4981122e5f0e1f52737f666d34fffaaf2e805
tiagocuzzuolnunes <tiagocuzzuolnunes@gmail.com>
2025-04-11 12:06:12 -0300

    feat: add header

---
+++ src/components/HamburgerMenu.jsx
@@ -0,0 +1,18 @@
+export default function HamburgerMenu() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="w-8 h-8"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+            />
+        </svg>
+    )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,17 @@
+import Logo from "@/components/Logo.jsx"
+import HamburgerMenu from "./HamburgerMenu"
+import LanguageIcon from "./LanguageIcon"
+
 export default function Header() {
-    <section className="header-container">
-        
-    </section>
+    return (<header className="w-full h-[70px] items-center justify-between flex flex-row flex-nowrap px-4">
+        <div className="flex items-center justify-between gap-2">
+            <Logo />
+            <p>EcoTrack</p>
+        </div>
+        <div className="flex items-center justify-between gap-2">
+            <LanguageIcon />
+            <HamburgerMenu />
+        </div>
+    </header>
+    )
 }
