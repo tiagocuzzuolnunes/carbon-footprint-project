cca917a78210638aaa3e1c0958a00d71e70c1752
tiagocuzzuolnunes <tiagocuzzuolnunes@gmail.com>
2025-04-11 11:05:29 -0300

    chore: change structure and clean startup next layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins, Montserrat } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const goPoppins = Poppins({
+  variable: "--font-poppins",
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const goMontserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
 });
 
@@ -20,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${goPoppins.variable} ${goMontserrat.variable} antialiased`}
       >
         {children}
       </body>
