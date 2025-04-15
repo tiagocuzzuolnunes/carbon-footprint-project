d7d9b6ddbc205b4523676c9b12f81c713c8519fe
tiagocuzzuolnunes <tiagocuzzuolnunes@gmail.com>
2025-04-15 11:24:48 -0300

    Cta Section update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
-export default function Button(type, content, isMain = false) {
+export default function Button({type, content, isMain = false}) {
     return (
-        <button type={type} className={`${isMain ? 'bg-primary' : 'bg-transparent'} w-full h-24 text-center rounded-xl`}>
+        <button type={type} className={`${isMain ? 'bg-primary' : 'bg-transparent'} ${isMain ? 'text-white' : 'text-black'} ${isMain ? '' : 'border-1 border-black'} w-full h-12 mb-3 text-center rounded-xl`}>
             {content}
         </button>
     )
--- src/components/CtaSection.jsx
+++ src/components/CtaSection.jsx
@@ -6,7 +6,8 @@ export default function CtaSection() {
             <img src="/banner.jpg" alt="Picture of futuristic nature harmony with society" className="w-full rounded-xl" />
             <h1 className="pt-5 text-center text-3xl text-colorForText">Calculate Your Business Carbon Footprint</h1>
             <p className="py-5 text-center text-md text-colorForText">Make your business sustainable and eco- friendly</p>
-            <Button isMain='true' type='button' content='Start Now' />
+            <Button isMain={true} type='button' content='Start Now' />
+            <Button isMain={false} type='button' content='Learn More' />
         </section>
     )
 }
