dcfcbd3d78cd1c0221127bca6d7f106fa8174402
tiagocuzzuolnunes <tiagocuzzuolnunes@gmail.com>
2025-04-17 10:28:02 -0300

    feat: create calculation page

---
+++ src/app/api/route.js
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+    return NextResponse.json({
+        hello: "world",
+    })
+}
---
+++ src/app/calculate/page.js
@@ -0,0 +1,15 @@
+import CtaSection from "@/components/CtaSection";
+import Header from "@/components/Header";
+import InformativeSection from "@/components/InformativeSection";
+import LearnSection from "@/components/LearnSection";
+import Image from "next/image";
+
+export default function Calculate() {
+    return (
+        <div>
+            <CtaSection />
+            
+            <LearnSection />
+        </div>
+    )
+}
