773eb7395f6f4367d869772020129562dcf60910
tiagocuzzuolnunes <tiagocuzzuolnunes@gmail.com>
2025-04-24 16:35:26 -0300

    feat:add header responsiveness

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({type, content, isMain = false}) {
     return (
-        <button type={type} className={`${isMain ? 'bg-primary' : 'bg-transparent'} ${isMain ? 'text-white' : 'text-black'} ${isMain ? '' : 'border-1 border-black'} w-full h-12 mb-3 text-center rounded-xl`}>
+        <button type={type} className={`${isMain ? 'bg-primary' : 'bg-transparent'} ${isMain ? 'text-white' : 'text-black'} ${isMain ? '' : 'border-1 border-black'} w-full h-12 px-3 text-center rounded-xl cursor-pointer hover:text-colorForText active:text-colorForText`}>
             {content}
         </button>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,34 @@
 import Logo from "@/components/Logo.jsx"
 import HamburgerMenu from "./HamburgerMenu"
 import LanguageIcon from "./LanguageIcon"
+import Button from "./Button"
 
 export default function Header() {
 
-    return (<header className="w-full h-[70px] items-center justify-between flex flex-row flex-nowrap px-5 shadow-md">
+    return (<header className="w-full h-[70px] md:h-[85px] items-center justify-between flex flex-row flex-nowrap px-5 shadow-md">
         <div className="flex items-center justify-between gap-2">
             <Logo />
             <p className="text-primary font-bold">EcoTrack</p>
         </div>
         <div className="flex items-center justify-between gap-4">
-            <div className="flex items-center justify-between gap-1">
+            <div className="hidden md:flex px-4">
+                <ul className="flex gap-6 items-center">
+                    <li className="py-2"><a className="hover:text-lighterText" href="#">Learn More</a></li>
+                    <li className="py-2"><a className="hover:text-lighterText" href="#">Contact</a></li>
+                    <li className="py-2"><a className="hover:text-lighterText" href="#">Education</a></li>
+                    <li className="py-2"><a className="hover:text-lighterText" href="#">About</a></li>
+                    <li className="py-2"><Button type="submit" content='Start Now' isMain={true}></Button></li>
+                </ul>
+            </div>
+            <div className="flex items-center justify-between gap-1 cursor-pointer">
                 <LanguageIcon />
-                <select name="languageOption" id="languageOption">
+                <select className="cursor-pointer" name="languageOption" id="languageOption">
                     <option value="en">EN</option>
                     <option value="is">IS</option>
                     <option value="pt">PT</option>
                 </select>
             </div>
+
             <HamburgerMenu />
         </div>
     </header>
