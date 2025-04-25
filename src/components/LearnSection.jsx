2f1f6d604080eb02f4c1419d9c6e85c92acf2933
tiagocuzzuolnunes <tiagocuzzuolnunes@gmail.com>
2025-04-25 10:56:09 -0300

    feat: deleting comments

--- src/components/LearnSection.jsx
+++ src/components/LearnSection.jsx
@@ -2,12 +2,6 @@ import Button from "./Button"
 
 export default function LearnSection() {
     return (
-        // <section className=" flex flex-col px-5 py-12">
-        //     <img src="/learn2.png" alt="Picture of person holding plant" className="w-full rounded-xl" />
-        //     <h1 className="pt-5 text-left text-3xl text-colorForText">Learn & Grow</h1>
-        //     <p className="py-5 text-left text-md text-colorForText">Access our educational resources to become a sustainable business leader</p>
-        //     <Button isMain={true} type='button' content='View Resources' />
-        // </section>
         <section className="grid md:grid-cols-2 flex-col place-items-center p-5">
             <img src="/learn2.png" alt="Illustration of person studying with a computer" className="md:hidden w-full rounded-xl" />
             <div className="w-[90%]">
