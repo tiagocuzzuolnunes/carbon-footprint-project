5d640324cf71f89f8a90040ca7016a3defb13910
tiagocuzzuolnunes <tiagocuzzuolnunes@gmail.com>
2025-04-25 10:36:55 -0300

    feat: add responsiveness to Cta and Informative Sections

--- src/components/CtaSection.jsx
+++ src/components/CtaSection.jsx
@@ -4,13 +4,17 @@ import Button from "./Button"
 
 export default function CtaSection() {
 
-    return(
-        <section className="flex flex-col items-center p-5">
+    return (
+        <section className="grid md:grid-cols-2 flex-col place-items-center p-5">
             <img src="/banner2.png" alt="Picture of futuristic nature harmony with society" className="w-full rounded-xl" />
-            <h1 className="pt-5 text-center text-3xl text-colorForText">Calculate Your Business Carbon Footprint</h1>
-            <p className="py-5 text-center text-md text-colorForText">Make your business sustainable and eco-friendly</p>
-            <Button isMain={true} type='button' content='Start Now' />
-            <Button isMain={false} type='button' content='Learn More' />
+            <div className="w-[90%]">
+                <h1 className="pt-5 text-center text-3xl text-colorForText">Calculate Your Business Carbon Footprint</h1>
+                <p className="py-5 text-center text-md text-colorForText">Make your business sustainable and eco-friendly</p>
+                <div className="flex flex-col gap-3 w-full">
+                    <Button isMain={true} type='button' content='Start Now' />
+                    <Button isMain={false} type='button' content='Learn More' />
+                </div>
+            </div>
         </section>
     )
 }
